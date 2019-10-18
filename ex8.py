112e811db4fe3be186354d986491f1a35d5ef89f
Pedro Raimundo <pedro.raimundo@ua.pt>
2019-10-18 16:13:07 +0100

    Fixed bugs

--- ex8.py
+++ ex8.py
@@ -12,9 +12,11 @@ signal_ori_right = 0.0
 signal_err_right = 0.0
 max_error_left = 0
 max_error_right = 0
+SNR_left = 0
+SNR_right = 0
 
 for i in range(0, obj.getnframes()):
-    print("Frame:", i)
+    #print("Frame:", i)
     ori = struct.unpack("<2h", obj.readframes(1))
     enc = struct.unpack("<2h", obj2.readframes(1))
 
@@ -32,8 +34,16 @@ for i in range(0, obj.getnframes()):
     signal_ori_right += pow(int(ori[1]), 2)
     signal_err_right += pow(int(ori[1]) - int(enc[1]), 2)
 
-SNR_left = 10 * math.log10(signal_ori_left / signal_err_left)
-SNR_right = 10 * math.log10(signal_ori_right / signal_err_right)
+if signal_err_left != 0:
+    SNR_left = 10 * math.log10(signal_ori_left / signal_err_left)
+else:
+    SNR_left = 0
+
+if signal_err_right != 0:
+    SNR_right = 10 * math.log10(signal_ori_right / signal_err_right)
+else:
+    SNR_left = 0
+
 print("SNR left:", SNR_left)
 print("SNR right:", SNR_right)
 print("SNR all:", (SNR_left + SNR_right ) / 2)
